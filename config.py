651a73724c15e53f081bc35058f369241ab2330c
Eli Goodman <eli.m.goodman@gmail.com>
2013-04-21 15:29:51 -0400

    Login working

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from os import environ
 
 local = {
     "PW_SALT": "$2a$12$0EqjsotuAMWiN63dRLsnMe",
-    "DATABASE_URL": "sqlite:///test.db"
+    "DATABASE_URL": "sqlite:///test.db",
+    "TOKEN_SECRET": "8qEJw9Nq63T3VZjvdugntU"
 }
 
 def get_config(key):
--- index.py
+++ index.py
@@ -1,14 +1,22 @@
 import os
 from config import get_config
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 import bcrypt
 from flask.ext.heroku import Heroku
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from flask.ext.wtf import Form
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = get_config('DATABASE_URL')
-
+app.secret_key = get_config("TOKEN_SECRET")
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.setup_app(app)
+login_manager.login_view = "login"
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,17 +36,80 @@ class User(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.first_name
 
-db.create_all()
+    def is_authenticated(self):
+        return True
+    
+    def is_anonymous(self):
+        return False
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+@login_manager.user_loader
+def load_user(userid):
+    id = int(userid)
+    return User.query.filter(User.id == id).one()
+
+class RegistrationForm(Form):
+    first_name = TextField('First Name')
+    last_name = TextField('Last Name')
+    email = TextField('Email Address')
+    password = PasswordField('Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+class LoginForm(Form):
+    email = TextField('Email Address', [validators.Required()])
+    password = PasswordField('Password', [validators.Required()])
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        salt = get_config("PW_SALT")
+    
+        user = User(
+            form.first_name.data, 
+            form.last_name.data, 
+            form.password.data,
+            form.email.data
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('register.jinja', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        salt = get_config("PW_SALT")
+        password_hash = bcrypt.hashpw(form.password.data, salt)
+
+        clause = and_(User.email == form.email.data, User.password_hash == password_hash)
+        print form.email.data
+        user = User.query.filter(clause).one()
+
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.jinja', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 @app.route('/')
-def hello():
-    u = User("Eli", "Goodman", "password", "eli.m.goodman@gmail.com")
-    db.session.add(u)
-    db.session.commit()
-
-    users = User.query.all()
-    print users
-    return 'Hi'
+def index():
+    return render_template('index.jinja', user=current_user)
 
 if __name__ == "__main__":
     app.run(debug=True)
